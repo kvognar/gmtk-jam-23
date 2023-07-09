98f94c41a1c68cbf43faa723d2dc93647aadb4ab
Kevin Vognar <kvognar@gmail.com>
2023-07-09 01:27:03 -0700

    added some title screen music

--- UI/title_screen.gd
+++ UI/title_screen.gd
@@ -5,8 +5,6 @@ signal change_to_scene(next_scene: PackedScene)
 
 func _process(_delta):
 	if Input.is_key_pressed(KEY_Q) and Input.is_key_pressed(KEY_P):
-		print_debug("qp")
 		if !exiting:
-			print_debug("start the game!")
 			exiting = true
 			change_to_scene.emit(load("res://Scenes/intro.tscn"))
