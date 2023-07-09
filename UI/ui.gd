7148dd6ceb509946d694794de6956d0a7f34fa98
Jeff Wong <awole20@gmail.com>
2023-07-08 23:15:30 -0700

    add barks for swatting mosquito

--- UI/ui.gd
+++ UI/ui.gd
@@ -7,17 +7,26 @@ var balloon: Node
 func _ready():
 	balloon = $ColorRect2/Balloon
 	get_node("/root/ScoreKeeper").boss_health_changed.connect(_on_big_boss_was_hit)
+	get_node("/root/ScoreKeeper").lives_changed.connect(_on_lives_changed)
 	balloon.dialog_ended.connect(_on_dialog_end)
 
 func _on_dialog_end() -> void:
 	get_node("/root/Dialog").in_dialog = false
 	await get_tree().create_timer(5).timeout
 	get_node("/root/Dialog").recently_displayed = false
+	
+func _on_lives_changed(lives):
+	print_debug(lives)
+	if lives == 2:
+		show_dialogue("mosquito_death_1", true)
+	elif lives == 1:
+		show_dialogue("mosquito_death_2", true)
+	elif lives == 0:
+		show_dialogue("mosquito_death_3", true)
 
 func _on_big_boss_was_hit(health):
 	if not get_node("/root/Dialog").recently_displayed:
 		var health_percentage = ((health * 100) / get_node("/root/ScoreKeeper").MAX_HEALTH)
-		print_debug(health_percentage)
 		if health_percentage > 60:
 			show_dialogue("doing_well")
 		elif health_percentage > 30:
